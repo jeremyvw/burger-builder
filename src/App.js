1f76924093333619063b637dbbb65d14ac2c8c4a
Jeremy Vijay Wongso <37033334+jeremyvw@users.noreply.github.com>
2021-01-20 20:10:53 +0700

    Create layout component
    Also change font family

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 
+import Layout from './components/Layout/Layout';
+
 class App extends Component {
   render() {
-    return <div>Burger Builder</div>;
+    return (
+      <div>
+        <Layout>
+          <p>Test</p>
+        </Layout>
+      </div>
+    );
   }
 }
 
