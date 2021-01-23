4a3ac7b5010026a349e19a0e6593dae67ef59a2e
Jeremy Vijay Wongso <37033334+jeremyvw@users.noreply.github.com>
2021-01-23 22:05:18 +0700

    Output multiple build controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import BuildControl from '../../components/Burger/BuildControls/BuildControl/BuildControl';
 
 class BurgerBuilder extends Component {
   state = {
@@ -17,7 +19,7 @@ class BurgerBuilder extends Component {
     return (
       <Auxiliary>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build Controls</div>
+        <BuildControls />
       </Auxiliary>
     );
   }
