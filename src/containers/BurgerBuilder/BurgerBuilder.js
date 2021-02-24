1566a1077c987fe2623c078136fe860438e06047
Jeremy Vijay Wongso <37033334+jeremyvw@users.noreply.github.com>
2021-02-24 20:46:25 +0700

    Add checkout page navigation

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,6 +27,7 @@ class BurgerBuilder extends Component {
   };
 
   componentDidMount() {
+    console.log(this.props);
     axios
       .get(
         'https://react-my-burger-3a1ad-default-rtdb.firebaseio.com/ingredients.json'
@@ -90,31 +91,32 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    // alert('You continue!');
-    this.setState({ loading: true });
-    // Firebase endpoint
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Jeremy Vijay Wongso',
-        address: {
-          street: 'Teststreet 1',
-          zipCode: '123456',
-          country: 'Indonesia',
-        },
-        email: 'test@test.com',
-      },
-      deliveryMethod: 'fastest',
-    };
-    axios
-      .post('/orders.json', order)
-      .then((response) => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch((error) => {
-        this.setState({ loading: false, purchasing: false });
-      });
+    // // alert('You continue!');
+    // this.setState({ loading: true });
+    // // Firebase endpoint
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'Jeremy Vijay Wongso',
+    //     address: {
+    //       street: 'Teststreet 1',
+    //       zipCode: '123456',
+    //       country: 'Indonesia',
+    //     },
+    //     email: 'test@test.com',
+    //   },
+    //   deliveryMethod: 'fastest',
+    // };
+    // axios
+    //   .post('/orders.json', order)
+    //   .then((response) => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   })
+    //   .catch((error) => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   });
+    this.props.history.push('/checkout');
   };
 
   render() {
