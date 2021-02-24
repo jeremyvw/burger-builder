206c03667fcf1df04c367be8046089ad03453af9
Jeremy Vijay Wongso <37033334+jeremyvw@users.noreply.github.com>
2021-02-25 01:25:38 +0700

    Display the orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,13 +2,39 @@ import React from 'react';
 
 import classes from './Order.module.css';
 
-const Order = (props) => (
-  <div className={classes.Order}>
-    <p>Ingredients: </p>
-    <p>
-      Price: <strong>USD: 4.5</strong>
-    </p>
-  </div>
-);
+const Order = (props) => {
+  const ingredients = [];
+
+  for (let ingredientName in props.ingredients) {
+    ingredients.push({
+      name: ingredientName,
+      amount: props.ingredients[ingredientName],
+    });
+  }
+
+  const ingredientOutput = ingredients.map((ig) => (
+    <span
+      style={{
+        textTransform: 'capitalize',
+        display: 'inline-block',
+        margin: '0 8px',
+        border: '1px solid #ccc',
+        padding: '5px',
+      }}
+      key={ig.name}
+    >
+      {ig.name} ({ig.amount})
+    </span>
+  ));
+
+  return (
+    <div className={classes.Order}>
+      <p>Ingredients: {ingredientOutput}</p>
+      <p>
+        Price: <strong>{Number.parseFloat(props.price).toFixed(2)}</strong>
+      </p>
+    </div>
+  );
+};
 
 export default Order;
